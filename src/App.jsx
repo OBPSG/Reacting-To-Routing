fbfad1442bf9a78dfd8f30875e5d2a5ecaac6bcc
OBPSG <vbeasupergamer@gmail.com>
2022-03-19 11:44:21 -0500

    Got movie list to display

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import FilmEntry from "./components/FilmEntry.jsx";
 
 const App = () => {
+    const [films, setFilms] = React.useState([]);
+
+    useEffect(() => {
+        fetch("https://ghibliapi.herokuapp.com/films")
+        .then(response => {
+            return response.json();
+        })
+        .then((films) =>
+        { //console.log(films)
+            setFilms(films);
+        });
+    }, []);
+
     return(
         <div>
-            <h1>Hello from App Component!</h1>
+            <div className="d-flex justify-content-center">
+                <img src="https://ghibliapi.herokuapp.com/images/logo.svg" alt="Studio Ghibli API Logo"></img>
+            </div>
+            
+            <h1 className="text-center">Studio Ghibli Films And People</h1>
+            <div class="row">
+                {films.map((film) => {
+                return <FilmEntry movieBanner={film.movie_banner}
+                movieTitle = {film.title}
+                description = {film.description}>    
+                </FilmEntry>
+                
+                })}
+            </div>
+            
         </div>
-    )
+    );
 }
 
 export default App;
